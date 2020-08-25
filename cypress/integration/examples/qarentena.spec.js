b6e95cbf7e62285471adc51ffa10388eaa384c8e
joaolucasf <joaolucasfernandes@gmail.com>
2020-08-25 19:19:26 -0300

    Adding specific screen scenarios

--- cypress/integration/examples/qarentena.spec.js
+++ cypress/integration/examples/qarentena.spec.js
@@ -4,33 +4,41 @@ context('qarentena', () => {
   
     it('Check homepage', () => {
         cy.visit(base_url)
-        cy.percySnapshot();
+        cy.percySnapshot('Homepage responsive test');
     })
-  
+
     it('Check blog page', () => {
         cy.visit(base_url+'/blog-2')
-        cy.percySnapshot();
+        cy.percySnapshot('Blog Snapshot');
     })
   
     it('Check about page', () => {
         cy.visit(base_url+'/about')
-        cy.percySnapshot();
+        cy.percySnapshot('About Snapshot');
     })
 
     it('Check contact page', () => {
         cy.visit(base_url+'/contact')
-        cy.percySnapshot();
+        cy.percySnapshot('Contact Snapshot');
     })
 
     it('Check register page', () => {
         cy.visit(base_url+'/cadastro')
-        cy.percySnapshot();
+        cy.percySnapshot('Registar Snapshot');
     })
 
     it('Check search page', () => {
         cy.visit(base_url+'/pesquisa')
-        cy.percySnapshot();
+        cy.percySnapshot('Search Snapshot');
     })
 
-  })
+/*
+    it('Check homepage', () => {
+        cy.visit(base_url)
+        cy.percySnapshot('Homepage responsive test', { widths: [768, 992, 1200] });
+        
+    })
+*/
+
+})
   
